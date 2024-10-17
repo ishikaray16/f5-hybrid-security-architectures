42b8b2088f762bc206f3602a4213df6ac0286c14
Ishika RAY <i.ray@f5.com>
2024-10-17 18:29:15 +0530

    duplicated output external_name in nap/outputs.tf

--- nap/outputs.tf
+++ nap/outputs.tf
@@ -11,3 +11,6 @@ output "nap_deployment_name" {
     value = try (helm_release.nginx-plus-ingress[0].name, helm_release.nginx-plus-ingresslink[0].name)
     sensitive = true
 }
+output "external_name" {
+    value = try(data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname, null)
+}
