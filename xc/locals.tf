9d8804b728d587fe754f562e4076b114be1ff055
Ishika RAY <i.ray@f5.com>
2024-10-23 12:37:31 +0530

    changes origin_nginx in xc/locals.tf

--- xc/locals.tf
+++ xc/locals.tf
@@ -2,7 +2,8 @@ locals {
   project_prefix = data.tfe_outputs.infra.values.project_prefix
   build_suffix = data.tfe_outputs.infra.values.build_suffix
   origin_bigip = try(data.tfe_outputs.bigip[0].values.bigip_public_vip, "")
-  origin_nginx = try(data.tfe_outputs.nap[0].values.external_name, data.tfe_outputs.nic[0].values.external_name, "")
+  # origin_nginx = try(data.tfe_outputs.nap[0].values.external_name, data.tfe_outputs.nic[0].values.external_name, "")
+  origin_nginx = ""
   origin_server = "${coalesce(local.origin_bigip, local.origin_nginx, var.serviceName)}"
   origin_port = try(data.tfe_outputs.nap[0].values.external_port, data.tfe_outputs.nic[0].values.external_port, "80")
   dns_origin_pool = local.origin_nginx != "" ? true : false 
